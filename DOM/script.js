82183c5e91b9cc3c6eb88958c7d0419cd0a2d3d1
Vignesh D <vignesh.d9123@gmail.com>
2025-06-16 21:21:16 +0530

    DOM-p1

---
+++ DOM/script.js
@@ -0,0 +1,12 @@
+function changeColor (){
+    document.getElementById("main-heading").style.color = "black";
+}
+
+
+function changeBGColor(){
+    document.querySelector(".info-text")
+}
+
+function changeText(){
+    document.getElementById("change-text").innerText = "Hi";
+}
