3f1adcbd4194e953924ff60220ea8796d281bf52
Vignesh D <vignesh.d9123@gmail.com>
2025-06-19 21:18:15 +0530

    Scaffolding react

---
+++ React-Intro/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+ return (
+     <div>
+           <div>Hello world<p>I an child of hw</p></div>
+            <div>Hey there</div>
+        </div>    
+      )
+  
+ 
+}
+
+export default App
